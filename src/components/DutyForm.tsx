8c20b043e6cade0fcae6c587e4fc1b6806e7655d
tutski3 <joseph.tusay123@gmail.com>
2023-10-31 22:11:36 +0800

    Update DutyForm.tsx

--- src/components/DutyForm.tsx
+++ src/components/DutyForm.tsx
@@ -15,9 +15,8 @@ const centerStyles: React.CSSProperties = {
 const DutyForm: React.FC = () => {
   const [name, setName] = useState('');
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-
+  const handleSubmit = async () => {
+    // Your mutation query and variables
     const mutation = `
       mutation CreateDuty($name: String!) {
         addDuty(name: $name) {
@@ -31,43 +30,41 @@ const DutyForm: React.FC = () => {
       name: name,
     };
 
-    axios({
-      url: 'http://localhost:4000',
-      method: 'post',
-      data: {
+    try {
+      
+      const response = await axios.post('http://localhost:4000', {
         query: mutation,
         variables: variables,
-      },
-    })
-      .then((response) => {
-        console.log('Mutation response:', response.data);
-      })
-      .catch((error) => {
-        console.error('Mutation error:', error);
       });
+
+    
+      console.log('Mutation response:', response.data);
+    } catch (error) {
+     
+      console.error('Mutation error:', error);
+    }
   };
 
   return (
     <div style={centerStyles}>
-    <Form onFinish={handleSubmit}>
-      <Item>
-        <Input
-          type="text"
-          placeholder="Add or update duty"
-          value={name}
-          onChange={e => setName(e.target.value)}
-          style={{ width: '200px' }}
-        />
-      </Item>
-      <Item>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Item>
-    </Form>
-  </div>
+      <Form onFinish={handleSubmit}>
+        <Item>
+          <Input
+            type="text"
+            placeholder="Add or update duty"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            style={{ width: '200px' }}
+          />
+        </Item>
+        <Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Item>
+      </Form>
+    </div>
   );
 };
-
 
 export default DutyForm;
